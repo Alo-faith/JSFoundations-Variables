3a6faba9b8966e6045f07d2e0f08d2607fe0b576
alo-faith <alo.faith@gmail.com>
2020-06-15 18:06:56 +0300

    new project

--- story.js
+++ story.js
@@ -1,7 +1,12 @@
 // Define the name here on line 2
-
+let name = "Eman";
 // Define the age here on line 4
-
+let age = 38;
 // Define the hobby here on line 6
-
+let hobby = "codding";
 // Print the story here on line 8
+console.log(
+  `Mrs. ${name} in 33 year, you will be ${
+    age + 33
+  } years old . By then you'll have decades of ${hobby} practice! `
+);
